600191114ea4d4808afc914d8b441b3cbc97076e
Yogesh Banger <bangerjaat111@gmail.com>
2026-04-17 12:20:53 +0530

    hiho

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import routes from './routes/routes.js';
 
+dotenv.config()
 const app=express()
 app.use(cors())
 app.use('/',routes)
-
+mongoose.connect('mongodb+srv://Yogesh:yogeshbanger@cluster0.xyohb54.mongodb.net/')
+.then(()=>console.log('mongose is connected'))
+.catch((err)=>console.log('mongoose is not connected',err.message))
 const port =8080 || process.env.PORT
 app.listen(port , ()=>{console.log(`server is running on port :http://localhost:${port}`)})
